fbfbde45f7f771981697ac574120084f898acb4c
nlikhopoi <junetomato@yandex.ru>
2021-02-23 00:01:49 +0800

    add (redux): refactor with react-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer, { plusAction } from './store/counter';
+
+const store = new createStore( rootReducer );
+
+console.log( '### store', store.getState() );
+store.subscribe(( () => console.log( '### store subscribe', store.getState() ) ));
+
+store.dispatch( plusAction( 5 ) );
 
 ReactDOM.render(
   <React.StrictMode>
