893b17db1d64f1c8f832828c660b4601960ee896
nlikhopoi <junetomato@yandex.ru>
2021-03-13 22:19:18 +0800

    add (Modal): modal form and logic

--- src/components/MenuNavbar/index.jsx
+++ src/components/MenuNavbar/index.jsx
@@ -1,13 +1,19 @@
-import Menu from './Menu'
-import NavBar from './NavBar'
-import { useState } from 'react'
+import Menu from './Menu';
+import NavBar from './NavBar';
+import { useState } from 'react';
+import Modal from '../Modal';
 
 function MenuNavbar({ bgActive }) {
 
     const [ isActive, setActive ] = useState( null );
+    const [ isOpenModal, setOpenModal ] = useState( false );
 
     const handleSetActive = () => {
         setActive( prevState => !prevState );
+    }
+
+    const handleClickLogin = () => {
+        setOpenModal( prevState => !prevState );
     }
 
     return (
@@ -20,7 +26,15 @@ function MenuNavbar({ bgActive }) {
                 isActive={ isActive }
                 onSetActive={ handleSetActive }
                 bgActive={ bgActive }
+                onClickLogin={ handleClickLogin }
                 />
+            <Modal
+                isOpen={ isOpenModal }
+                title="Log In..."
+                onCloseModal={ handleClickLogin }
+                >
+                Some Text Here...
+            </Modal>
         </>
     )
 }
